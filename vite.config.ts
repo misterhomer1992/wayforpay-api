cdd7ebb892f1ce734e81d4dec057cba67decfcc6
mariuschik <misterhomer1992@gmail.com>
2024-05-10 17:09:30 +0300

    Add typings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-export default {
+export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/main.ts'),
@@ -16,4 +18,5 @@ export default {
             },
         },
     },
-};
+    plugins: [dts()],
+});
